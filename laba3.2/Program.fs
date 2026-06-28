a356a903e8e5e9e45588f11bdacfdff0c79b942c
Samsheiko <sheiko22@mail.ru>
2026-06-29 02:02:06 +0500

    Add files via upload

---
+++ laba3.2/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let toInt = function
+
+    | "i" -> 1    | "ii" -> 2   | "iii" -> 3
+    | "iv" -> 4   | "v" -> 5    | "vi" -> 6
+    | "vii" -> 7  | "viii" -> 8 | "ix" -> 9
+    | _ -> 0
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите числа: "
+    
+    Console.ReadLine().ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+
+    |> Seq.fold (fun acc x -> acc + toInt x) 0
+    |> printfn "Сумма: %d"
+
+    0
