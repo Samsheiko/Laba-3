793d7ab3bc09e0647dc4f9a3e0da4961626e90fd
Samsheiko <sheiko22@mail.ru>
2026-06-29 01:52:22 +0500

    Add files via upload

---
+++ Laba3.1/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let checkEven numbers =
+    numbers |> Seq.map (fun x -> x % 2 = 0)
+
+printf "Введите числа через пробел: "
+let input = Console.ReadLine()
+
+let result = 
+    input.Split ' ' // Простое разделение по пробелу
+
+    |> Seq.map int
+    |> checkEven
+
+printfn "%A" result
+
