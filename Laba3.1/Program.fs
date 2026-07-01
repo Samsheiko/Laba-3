038ec544e7f03a56d0ddf1d1d8f7ac5688583916
Samsheiko <sheiko22@mail.ru>
2026-07-01 21:55:25 +0500

    Add files via upload

---
+++ Laba3.1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let isEven x = x % 2 = 0
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите количество чисел: "
+    let n = int (Console.ReadLine())
+
+    let numbers =
+        Seq.init n (fun i ->
+            printf "Введите число %d: " (i + 1)
+            int (Console.ReadLine()))
+
+    let res = 
+        numbers 
+        |> Seq.map isEven 
+        |> Seq.toList
+
+    printfn "Результат: %A" res
+    0
